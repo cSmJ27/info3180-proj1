f5293ae74eb1727eee3ed22d40b9f0ce09d0b77b
cSmJ27 <christophe.s.james@gmail.com>
2015-02-28 02:23:23 +0000

    db info is now added

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mnmgqggkougpmr:6HJy8a3ssg3kDpwzdZZUfMKLrR@ec2-23-23-180-133.compute-1.amazonaws.com:5432/dfqcq2aq8vlaie'
 app.config['SECRET_KEY'] = "itsasecret"
 db = SQLAlchemy(app)
 
